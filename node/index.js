758485cac344bf7463bd9f8def7e668fb7b77859
Mukta Aphale <MAphale@whitehedge.com>
2016-03-22 19:56:09 +0530

    changes

--- node/index.js
+++ node/index.js
@@ -4,16 +4,13 @@ var express = require('express'),
 
 var app = express();
 
-console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
+console.log(process.env.REDIS_PORT + ':' + process.env.REDIS_ADDRESS);
 
-// APPROACH 1: Using environment variables created by Docker
-// var client = redis.createClient(
-// 	process.env.REDIS_PORT_6379_TCP_PORT,
-//   	process.env.REDIS_PORT_6379_TCP_ADDR
-// );
-
-// APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
-var client = redis.createClient('6379', 'redis');
+APPROACH 1: Using environment variables created by Docker
+var client = redis.createClient(
+	process.env.REDIS_PORT,
+  	process.env.REDIS_ADDRESS
+);
 
 
 app.get('/', function(req, res, next) {
